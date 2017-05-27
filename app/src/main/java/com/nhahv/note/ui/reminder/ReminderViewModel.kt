a85fa6f7fcc135178307a66ea8ce3e28b97f567c
nhahv0902 <nhahv0902@gmail.com>
2017-05-28 03:41:43 +0700

    task#5 Create UI and Logic Activity Reminders

--- app/src/main/java/com/nhahv/note/ui/reminder/ReminderViewModel.kt
+++ app/src/main/java/com/nhahv/note/ui/reminder/ReminderViewModel.kt
@@ -1,6 +1,8 @@
 package com.nhahv.note.ui.reminder
 
+import android.databinding.Bindable
 import android.support.v7.app.AppCompatActivity
+import com.android.databinding.library.baseAdapters.BR
 import com.nhahv.note.ui.BaseViewModel
 
 /**
@@ -10,12 +12,38 @@ import com.nhahv.note.ui.BaseViewModel
 
 class ReminderViewModel(activity: AppCompatActivity) : BaseViewModel(activity) {
 
+  val MAX_DAY = 7
+  @get: Bindable
+  var mIsSetup: Boolean = true
+    set(value) {
+      field = value
+      notifyPropertyChanged(BR.mIsSetup)
+    }
+
+  @get:Bindable
+  var mDayOfWeeks: BooleanArray = booleanArrayOf(false, false, false, false, false, false, false)
+    set(value) {
+      field = value
+      notifyPropertyChanged(BR.mDayOfWeeks)
+    }
+
+  init {
+    for (i in 0 until MAX_DAY) {
+      mDayOfWeeks[i] = false
+    }
+  }
 
   fun onClickWeekday(dayOfWeekData: Int) {
-    print(dayOfWeekData)
+    mDayOfWeeks[dayOfWeekData] = !mDayOfWeeks[dayOfWeekData]
+    notifyChange()
   }
 
   fun onPickDate() {
     println("Log")
+  }
+
+  fun onCheckTurnOnOffReminder(checked: Boolean) {
+    mIsSetup = checked
+
   }
 }
