1e62ab4c2bc17e648760bf2b453b44f9d1c62d5e
nhahv0902 <nhahv0902@gmail.com>
2017-05-28 21:26:33 +0700

    task#8 UI and Logic in Activity Note creation

--- app/src/main/java/com/nhahv/note/ui/loadpicture/LoaderPicture.kt
+++ app/src/main/java/com/nhahv/note/ui/loadpicture/LoaderPicture.kt
@@ -19,40 +19,40 @@ class LoaderPicture(context: Context) {
     private val MIME_TYPE = MediaStore.Images.Media.MIME_TYPE
     private val SELECTION = "$MIME_TYPE=? or $MIME_TYPE=?"
     private val IMAGE_PROJECTION = arrayOf(MediaStore.Images.Media.DATA,
-	MediaStore.Images.Media.DISPLAY_NAME, MediaStore.Images.Media.DATE_ADDED,
-	MediaStore.Images.ImageColumns.BUCKET_DISPLAY_NAME)
+            MediaStore.Images.Media.DISPLAY_NAME, MediaStore.Images.Media.DATE_ADDED,
+            MediaStore.Images.ImageColumns.BUCKET_DISPLAY_NAME)
 
     val mContext: Context = context
 
-    fun loadImages(): ArrayList<Folder> {
-	val folders: ArrayList<Folder> = ArrayList()
+    fun loadPictures(): ArrayList<Folder> {
+        val folders: ArrayList<Folder> = ArrayList()
 
-	val cursor: Cursor? = mContext.contentResolver.query(
-	    MediaStore.Images.Media.EXTERNAL_CONTENT_URI, IMAGE_PROJECTION, SELECTION,
-	    SELECTION_ARGS, IMAGE_PROJECTION[2] + " DESC", null)
+        val cursor: Cursor? = mContext.contentResolver.query(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, IMAGE_PROJECTION, SELECTION,
+                SELECTION_ARGS, IMAGE_PROJECTION[2] + " DESC", null)
 
-	if (cursor == null || !cursor.moveToFirst()) return folders
-	val indexPath = cursor.getColumnIndex(IMAGE_PROJECTION[0])
-	val indexFolder = cursor.getColumnIndex(IMAGE_PROJECTION[3])
-	var folderName: String?
-	var imagePath: String?
+        if (cursor == null || !cursor.moveToFirst()) return folders
+        val indexPath = cursor.getColumnIndex(IMAGE_PROJECTION[0])
+        val indexFolder = cursor.getColumnIndex(IMAGE_PROJECTION[3])
+        var folderName: String?
+        var imagePath: String?
 
-	val hashFolder: HashMap<String, Folder> = HashMap()
+        val hashFolder: HashMap<String, Folder> = HashMap()
 
-	while (!cursor.isAfterLast) {
-	    folderName = cursor.getString(indexFolder)
-	    imagePath = cursor.getString(indexPath)
+        while (!cursor.isAfterLast) {
+            folderName = cursor.getString(indexFolder)
+            imagePath = cursor.getString(indexPath)
 
-	    if (!hashFolder.containsKey(folderName)) {
-		hashFolder.put(folderName, Folder(folderName, ArrayList()))
-	    }
-	    hashFolder[folderName]?.images?.add(imagePath)
-	    cursor.moveToNext()
-	}
-	cursor.close()
-	for ((_, value) in hashFolder) {
-	    folders.add(value)
-	}
-	return folders
+            if (!hashFolder.containsKey(folderName)) {
+                hashFolder.put(folderName, Folder(folderName, ArrayList()))
+            }
+            hashFolder[folderName]?.images?.add(imagePath)
+            cursor.moveToNext()
+        }
+        cursor.close()
+        for ((_, value) in hashFolder) {
+            folders.add(value)
+        }
+        return folders
     }
 }
