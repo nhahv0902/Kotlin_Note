d7c0894295fa727d01c3ac562f496e6e3e3c0ee7
nhahv0902 <nhahv0902@gmail.com>
2017-05-28 14:49:57 +0700

    task#7 TimePicker in ReminderActivity

--- app/src/main/java/com/nhahv/note/util/Extension.kt
+++ app/src/main/java/com/nhahv/note/util/Extension.kt
@@ -3,7 +3,9 @@ package com.nhahv.note.util
 import android.app.Fragment
 import android.content.Context
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.widget.Toast
+import com.nhahv.note.util.DataUtil.NOTE_TAG
 
 /**
  * Created by Hoang Van Nha on 5/28/2017.
@@ -11,17 +13,21 @@ import android.widget.Toast
  */
 
 fun Fragment.toast(context: Context, message: String, duration: Int = Toast.LENGTH_SHORT) {
-  context.let { Toast.makeText(context, message, duration).show() }
+    context.let { Toast.makeText(context, message, duration).show() }
 }
 
 fun AppCompatActivity.toast(context: Context, message: String, duration: Int = Toast.LENGTH_SHORT) {
-  context.let { Toast.makeText(context, message, duration).show() }
+    context.let { Toast.makeText(context, message, duration).show() }
 }
 
 fun Context.toast(context: Context, message: String, duration: Int = Toast.LENGTH_SHORT) {
-  context.let { Toast.makeText(context, message, duration).show() }
+    context.let { Toast.makeText(context, message, duration).show() }
 }
 
 fun Context.toast(context: Context, message: Int, duration: Int = Toast.LENGTH_SHORT) {
-  context.let { Toast.makeText(context, message, duration).show() }
+    context.let { Toast.makeText(context, message, duration).show() }
+}
+
+fun Context.log(message: String) {
+    Log.d(NOTE_TAG, message)
 }
